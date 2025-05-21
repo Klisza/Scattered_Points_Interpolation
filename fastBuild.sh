450f2df3ead51091f4a3d65fc99c01cbd5524c7a
Jakub Kliszewski <jakliszewski@gmail.com>
2025-05-21 02:09:13 +0200

    Working on UI

--- fastBuild.sh
+++ fastBuild.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 rm -rf build/*
 cmake -B build/
+cd build/
+make -j
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,58 @@
-#include <polyscope/polyscope.h>
-#include <polyscope/surface_mesh.h>
-#include <igl/readOBJ.h>
-#include <igl/file_dialog_open.h>
-#include <Eigen/Core>
-#include <string>
 #include <iostream>
+#include <string>
 
-int main(int argc, char* argv[]) {
-  // 1) Initialize Polyscope
-  polyscope::init();
+#include <polyscope/point_cloud.h>
+#include <polyscope/surface_mesh.h>
+#include <igl/file_dialog_open.h>
+#include <igl/readOBJ.h>
 
-  // 2) Install a per-frame ImGui callback using Polyscope's state
-  polyscope::state::userCallback = []() {
-    // Draw a button in the upper-left corner
-    if (ImGui::Button("Import Mesh")) {
+#include <Eigen/Core>
+#include <sparse_interp/basis.h>
+#include <sparse_interp/curve.h>
+#include <sparse_interp/mesh_processing.h>
+#include <sparse_interp/energy.h>
+#include <test.h>
 
-      // 3) Ask the user for an OBJ file
-      std::string filename;
-      // filter: “OBJ files” \0*.obj\0
+using namespace SIBSplines;
+std::string filename;
+
+void interpCallback() {
+    if (ImGui::BeginTabBar("Settings")) {
+
+    }
+    // Load and display mesh / point cloud
+    if (ImGui::Button("Import Point Cloud")) {
       filename = igl::file_dialog_open();
-
-      // Check if a file was selected
       if (!filename.empty()) {
-        // 4) Read vertices & faces
         Eigen::MatrixXd V;
         Eigen::MatrixXi F;
         if (igl::readOBJ(filename, V, F)) {
-
-          // 5) Register with Polyscope
-          polyscope::registerSurfaceMesh("imported mesh", V, F);
+          polyscope::PointCloud* psCloud = polyscope::registerPointCloud("mesh points", V);
+          psCloud->setPointRadius(0.002);
+          psCloud->setPointRenderMode(polyscope::PointRenderMode::Sphere);
+          // polyscope::registerSurfaceMesh("imported mesh", V, F);
         } else {
-          // Log an error message to the console
           std::cerr << "Failed to read OBJ." << std::endl;
         }
       }
     }
-  };
+    if (ImGui::Button("Compute Mesh")) {
 
-  // 6) Show the window and enter the Polyscope loop
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+  polyscope::options::programName = "Surface Interpolation GUI";
+  polyscope::options::autocenterStructures = true;
+  polyscope::options::autoscaleStructures = true;
+  polyscope::init();
+
+  //run_ours();
+  // Run user callback to get the mesh
+  // TODO: Rewrite this to get the point cloud from the user instead of the triangle mesh
+  polyscope::state::userCallback = interpCallback;
+
   polyscope::show();
-
   return 0;
 }
